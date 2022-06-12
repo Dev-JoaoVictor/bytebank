1e326a33356a47f312c109105ed9d92ce418b4c4
João <joaovictor.ramos34@gmail.com>
2022-06-12 13:49:10 -0300

    Bytebank

---
+++ Curso 1/2-trabalhandoComvariaveis.js
@@ -0,0 +1,8 @@
+console.log("Trabalhando com Variáveis");
+//JS é case sensitiveis
+
+const idade = 29;
+const nome = "Ricardo"
+
+//não fazer 
+//mes = "Janeiro"
---
+++ Curso 3/ContaPoupanca.js
@@ -0,0 +1,7 @@
+import { Conta } from "./Conta.js";
+
+export class ContaPoupanca extends Conta{
+    constructor(saldoinicial, cliente, agencia) {
+        super(saldoinicial, cliente, agencia);
+    }
+}
